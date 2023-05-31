dcbd93ba2a0f24bec6aeeb1657f3b3aff42f4d6a
luchang <835498386@qq.com>
2023-05-31 20:02:06 +0800

    feat: 新建项目

--- src/view/project/index.tsx
+++ src/view/project/index.tsx
@@ -1,11 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Storage } from '@/shared';
-import { message } from 'antd';
+import { message, Modal, Form, Input, Button } from 'antd';
+import { Icon } from '@/components';
+import { data } from './data';
 
 const Project = () => {
   const navigate = useNavigate();
-
+  const [open, setOpen] = useState<boolean>(false);
+  const [form] = Form.useForm();
   useEffect(() => {
     const token = Storage.local.get('token');
     if (!token) {
@@ -22,31 +25,52 @@ const Project = () => {
   return (
     <div className="w-full h-full rounded-md bg-skin-bg-base p-2.5">
       <div className="w-full flex content-start flex-wrap h-full">
-        <div
-          className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4  h-[100px] border border-solid border-[#dce2ef] rounded cursor-pointer hover:shadow-lg flex items-center"
-          onClick={() => {
-            navigate(`/project/1`);
-          }}>
-          <div className="w-[100px] h-full object-cover">
-            <img
-              className="w-full h-full"
-              src="https://polaris-hd2.oss-cn-shanghai.aliyuncs.com/front_resources/general_project_cover_x1.png?x-oss-process=style/thumbnail_001"
-            />
-          </div>
-          <div className="w-full h-full flex flex-col justify-between flex-1 p-2">
-            <div className="flex justify-between items-center">
-              <span className="text-base">你是</span>
-              <div className="w-8 h-8 rounded-full overflow-hidden  object-cover">
-                <img
-                  className="w-full h-full"
-                  src="https://attachments.startable.cn/org_2566/project_0/folder_resource/2023/2/8/472794f76eef4d0fb9037a6a28b952e41675845999361.jpg?x-oss-process=image/resize,w_100"
-                />
+        {data.map(item => {
+          return (
+            <div
+              key={item.name}
+              className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4  h-[100px] border border-solid border-[#dce2ef] rounded cursor-pointer hover:shadow-lg flex items-center mr-5 mb-5"
+              onClick={() => {
+                navigate(`/project/1`);
+              }}>
+              <div className="w-[100px] h-full object-cover">
+                <img className="w-full h-full" src={item.projectImg} />
+              </div>
+              <div className="w-full h-full flex flex-col justify-between flex-1 p-2">
+                <div className="flex justify-between items-center">
+                  <span className="text-base">{item.name}</span>
+                  <div className="w-8 h-8 rounded-full overflow-hidden  object-cover">
+                    <img className="w-full h-full" src={item.creator.img} />
+                  </div>
+                </div>
+                <div className="w-full text-sm text-textGray">{item.desc}</div>
               </div>
             </div>
-            <div className="w-full text-sm text-textGray">项目描述</div>
+          );
+        })}
+        <div
+          className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/4  h-[100px] border border-solid border-[#dce2ef] rounded cursor-pointer hover:shadow-lg flex items-center mr-5 mb-5"
+          onClick={() => {
+            setOpen(true);
+          }}>
+          <div className="w-full h-full bg-[#f1f3f9] flex justify-center items-center text-lg text-violet">
+            <Icon type="icona-automationadd" className="mr-5" />
+            <span>新建项目</span>
           </div>
         </div>
       </div>
+      <Modal open={open}>
+        <Form form={form}>
+          <Form.Item label="项目名称">
+            <Input />
+          </Form.Item>
+          <Form.Item label="项目名称">
+            <Button type="primary" onClick={() => setOpen(false)}>
+              确认
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
