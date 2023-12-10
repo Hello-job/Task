56655b8b67dfc43e34118f5ada15756645571f7a
luchang <835498386@qq.com>
2023-12-10 13:55:34 +0800

    feat: eslint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,28 +14,28 @@ module.exports = {
         baseGray: '#e7ebf4', // 表格灰色
         textGray: '#6e7080', // 文字灰色
         skin: {
-            'text-primary': 'var(--color-text-primary)',
-            'text-white': 'var(--color-text-white)',
-            'text-base': 'var(--color-text-base)',
-            'text-gray': 'var(--color-text-gray)',
-            'text-light-gray': 'var(--color-text-light-gray)',
-            'text-light': 'var(--color-text-light)',
-            'hover-gray': 'var(--color-hover-gray)',
-            'bg-base': 'var(--color-bg-base)',
-            'bg-primary': 'var(--color-bg-primary)'
-          },
+          'text-primary': 'var(--color-text-primary)',
+          'text-white': 'var(--color-text-white)',
+          'text-base': 'var(--color-text-base)',
+          'text-gray': 'var(--color-text-gray)',
+          'text-light-gray': 'var(--color-text-light-gray)',
+          'text-light': 'var(--color-text-light)',
+          'hover-gray': 'var(--color-hover-gray)',
+          'bg-base': 'var(--color-bg-base)',
+          'bg-primary': 'var(--color-bg-primary)'
+        }
       },
       boxShadow: {
         base: '0 4px 8px rgba(0,0,0,.07), 0 8px 28px rgba(0,0,0,.09)'
       },
       backgroundColor: {
-        mask: 'rgba(0, 0, 0, 0.45)', 
+        mask: 'rgba(0, 0, 0, 0.45)',
         violetMask: 'rgb(123, 103, 238, 0.3)'
       },
       keyframes: {
         modalShow: {
-            '0%': { transform: 'scale(0)', opacity: 0},
-          '100%': { transform: 'scale(1)', opacity: 1},
+          '0%': { transform: 'scale(0)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 }
         }
       },
       animation: {
